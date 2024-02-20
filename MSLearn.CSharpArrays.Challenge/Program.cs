5d73d744d214daf9fcbc1be1b80c8f161b99a9e8
jra <julien.gault.50@gmail.com>
2024-02-20 15:47:12 +0100

    foreach on starting with B

--- MSLearn.CSharpArrays.Challenge/Program.cs
+++ MSLearn.CSharpArrays.Challenge/Program.cs
@@ -27,3 +27,11 @@ B179
 */
 
 string[] fraudulentOrderIDs = { "B123", "C234", "A345", "C15", "B177", "G3003", "C235", "B179" };
+
+foreach (string order in fraudulentOrderIDs)
+{
+    if (order.StartsWith("B"))
+    {
+        Console.WriteLine(order);
+    }
+}
