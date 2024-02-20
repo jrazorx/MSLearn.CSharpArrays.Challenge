4598daf3aa4584a17882fd12c72cef5cccc1414f
jra <julien.gault.50@gmail.com>
2024-02-20 15:46:23 +0100

    Instructions

--- MSLearn.CSharpArrays.Challenge/Program.cs
+++ MSLearn.CSharpArrays.Challenge/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿/*
+Declare an array and initialize it to contain the following elements:
+B123
+C234
+A345
+C15
+B177
+G3003
+C235
+B179
+These values represent the fraudulent Order ID data that your application use.
+
+Create a foreach statement to iterate through each element of your array.
+
+Report the Order IDs that start with the letter "B".
+You need to evaluate each element of the array. Report the potentially fraudulent Order IDs by detecting the orders that start with the letter "B". To determine whether or not an element starts with the letter "B", use the String.StartsWith() method. Here's a simple example of how to use the String.StartsWith() method that you can adapt for your code:
+string name = "Bob";
+if (name.StartsWith("B"))
+{
+    Console.WriteLine("The name starts with 'B'!");
+}
+
+Your output should match the following:
+B123
+B177
+B179
+*/
